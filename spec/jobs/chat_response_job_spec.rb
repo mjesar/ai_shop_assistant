319639b153e1096cacaca974c4f8d07ab50fda61
mjesar <mohammadalijaisar@gmail.com>
2026-09-03 01:35:12 +0500

    Add ChatResponseJob test, update messages request spec for async flow

--- spec/jobs/chat_response_job_spec.rb
+++ spec/jobs/chat_response_job_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ChatResponseJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "calls generate_response on the chat" do
+    chat = Chat.create!(model_id: 'gemini-3.6-flash')
+
+    allow(Chat).to receive(:find).with(chat.id).and_return(chat)
+    allow(chat).to receive(:generate_response)
+
+    ChatResponseJob.perform_now(chat.id, 'Hello')
+
+    expect(chat).to have_received(:generate_response).with('Hello')
+  end
 end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
   describe "POST /chats/:chat_id/messages" do
-    it "sends a message and redirects to the chat" do
+    it "saves the user message and enqueues a background job" do
       chat = Chat.create!(model_id: 'gemini-3.6-flash')
 
-      fake_response = double(content: 'Hello there!')
-      fake_llm_chat = double('RubyLLM::Chat')
-      allow(fake_llm_chat).to receive(:with_tool).and_return(fake_llm_chat)
-      allow(fake_llm_chat).to receive(:ask).and_return(fake_response)
-      allow(RubyLLM).to receive(:chat).and_return(fake_llm_chat)
+      expect {
+        post "/chats/#{chat.id}/messages", params: { content: 'Hi' }
+      }.to have_enqueued_job(ChatResponseJob).with(chat.id.to_s, 'Hi')
 
-      post "/chats/#{chat.id}/messages", params: { content: 'Hi' }
-
-      expect(response).to redirect_to(chat_path(chat))
-      expect(chat.messages.count).to eq(2)
+      expect(response).to have_http_status(:no_content)
+      expect(chat.messages.count).to eq(1)
+      expect(chat.messages.first.content).to eq('Hi')
     end
   end
 end
