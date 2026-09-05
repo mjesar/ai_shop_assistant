b1f68335fbac73e02ea64799a91c695f46f0216b
mjesar <mohammadalijaisar@gmail.com>
2026-09-05 18:56:05 +0500

    Add Mission Control: Jobs dashboard for monitoring background jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount MissionControl::Jobs::Engine, at: "/jobs"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :chats, only: [:index, :new, :show, :create, :destroy] do
     resources :messages, only: [:create]
